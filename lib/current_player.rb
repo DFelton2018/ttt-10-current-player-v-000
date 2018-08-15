8d8de45841cf76adaf67b6e4184328def1f0914e
Duane Felton <dfelton2010@gmail.com>
2018-08-15 15:09:09 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 def turn_count(board)
   counter = 0
@@ -16,4 +17,18 @@ def current_player(board)
   else
     return "O"
   end
+=======
+counter = 0
+def turn_count(board)
+  board.each do |token
+    if token == "X" || token == "O"
+      counter += 1
+end
+end
+end
+
+
+def current_player
+  
+>>>>>>> 60b901e67de46dd48b16297fedd6549377a57aa0
 end
